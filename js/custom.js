a1eba111a6171db1dc5384595fc280385ad8afee
Doyle See <doyleinigosee@gmail.com>
2025-09-25 16:53:33 -0700

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -23,8 +23,10 @@ $(document).ready(function(){
 		
 		// Trigger effects when user scrolls down to specific section
 		$('.effects').each( function(i){
-			var bottom_of_object = $(this).offset().top - ($(window).height()/2);
+			var bottom_of_object = $(this).offset().top + 200;
 			var bottom_of_window = $(window).scrollTop() + $(window).height();
+
+			console.log (bottom_of_window : bottom_of_object);
 			
 			if( bottom_of_window > bottom_of_object ){
 				if ($(this).hasClass('effects--left')) {
